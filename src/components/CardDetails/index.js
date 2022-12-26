a8116a864ffdeed9ff515c744e5f2e96958a74e3
laasyasree-glitch <laasyakothamasu@gmail.com>
2022-12-26 13:11:22 +0000

    starImage update

--- src/components/CardDetails/index.js
+++ src/components/CardDetails/index.js
@@ -1,4 +1,6 @@
 import {Link} from 'react-router-dom'
+import {AiFillStar} from 'react-icons/ai'
+
 import './index.css'
 
 const CardDetails = props => {
@@ -15,11 +17,19 @@ const CardDetails = props => {
   } = itemDetails
   return (
     <div className="eachJob">
-      <Link to={`/jobs/${id}`} className="title">
-        <img src={imageUrl} alt="website logo" />
-        <h1>{title}</h1>
-      </Link>
-      <p>{rating}</p>
+      <div className="title">
+        <Link to={`/jobs/${id}`}>
+          <img src={imageUrl} alt="website logo" className="websiteLogo" />
+        </Link>
+        <div>
+          <h1>{title}</h1>
+          <div className="title">
+            <AiFillStar className="star" />
+            <p>{rating}</p>
+          </div>
+        </div>
+      </div>
+
       <div className="sub-alignment">
         <p>{packagePerAnnum}</p>
         <p>{location}</p>
